3cad924c1bec253e17a116f31b74a137984d440a
TechSimplifide <ry0805502@gmail.com>
2025-06-22 16:54:51 +0530

    Created responsive weather app using HTML5, CSS3, JS with styled card and live API

---
+++ script.js
@@ -0,0 +1,70 @@
+let loader = document.querySelector(".loader");
+let btn = document.querySelector("#btn");
+let container = document.querySelector(".container");
+
+btn.addEventListener("click", () => {
+  let inputs = document.querySelector("#value").value.trim();
+  if (inputs === "") {
+    let cancelSub = document.querySelector(".sub");
+    loader.style.display = "none";
+    container.style.padding = "0%";
+    document.querySelector("#error").style.display = "inline-block";
+    cancelSub.style.padding = "0.3rem";
+    cancelSub.style.backgroundColor = "rgba(169, 177, 174, 0.87)";
+    cancelSub.style.borderRadius = "0.5em";
+    cancelSub.style.border = "0.1em solid rgb(200, 203, 202)";
+
+    document.querySelector("#cancle").style.display = "inline-block";
+    document.querySelector("#error").textContent = "⚠️City name cannot be empty!";
+    return;
+  }
+  loader.style.display = "block";
+  container.style.padding = "3%";
+  let url = `https://api.weatherapi.com/v1/current.json?key=33f7cf3cc1ce40679af130949250205&q=${inputs}&aqi=yes`;
+  fetch(url)
+    .then((response, reject) => {
+      return response.json();
+    })
+    .then((data) => {
+      container.innerHTML = `
+            <p class='time'>${data.location.localtime}</p>
+            <div class='location' >
+            <h3><img src='./map.png' alt='map icon'/>${data.location.name}</h3>
+            <h4><img src='region.png'>${data.location.region}</h4>
+            <p><img src='country.png'>${data.location.country}</p>
+            </div>
+            <div class='temprature'>
+            <p>${data.current.temp_c}<img src='celsius.png'></p>
+            <p>${data.current.temp_f}<img src='fahrenheit.png'></p>
+            </div>
+            <div class='humidity'>
+            <p>${data.current.humidity}<img src='humidity.png'></p>
+            </div>
+            <div class='condition'>
+           <p><img src="${data.current.condition.icon}"/></p>
+            <p>${data.current.condition.text}</p>
+            </div>
+            `;
+      loader.style.display = "none";
+      inputs.innerText = " ";
+    })
+    .catch((error) => console.log(error));
+});
+
+let btn1 = document.querySelector("#clear");
+btn1.addEventListener("click", () => {
+  document.querySelector("#value").value = "";
+
+  container.innerHTML = " ";
+  container.style.padding = "0px";
+});
+
+document.querySelector("#cancle").addEventListener("click", () => {
+  document.querySelector("#error").style.display = "none";
+  document.querySelector("#cancle").style.display = "none";
+  let subCancle = document.querySelector(".sub");
+  subCancle.style.backgroundColor = "none";
+  subCancle.style.borderRadius = "none";
+  subCancle.style.border = "none";
+  subCancle.style.padding = "0rem";
+});
